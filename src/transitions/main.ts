45cf2604923e4282c8adaa7b1ac119c4283bc5e0
Francisco <javiermartincuenca@gmail.com>
2024-06-24 23:13:59 +0200

    transition listeners

--- src/transitions/main.ts
+++ src/transitions/main.ts
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const element = document.querySelector('.square')!;
+  const element = document.getElementById('square')!;
 
   document.onclick = (event: MouseEvent) => {
     element.style.transform = `translateY(${event.clientY - element.clientHeight / 2}px) translateX(${event.clientX - element.clientWidth / 2}px)`;
+  };
+
+  document.ontransitionrun = (event: TransitionEvent) => {
+    console.log(event);
+  };
+
+  document.ontransitionstart = (event: TransitionEvent) => {
+    console.log(event);
+  };
+
+  document.ontransitionend = (event: TransitionEvent) => {
+    if (event.propertyName === 'transform') {
+      element.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
   };
 });
