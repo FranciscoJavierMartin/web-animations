7c8205d57cc201acdb61baf666f1099613adfa80
Francisco <javiermartincuenca@gmail.com>
2024-06-25 18:29:02 +0200

    Display animation

--- src/transitions/main.ts
+++ src/transitions/main.ts
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.getElementById('square')!;
+  const toggleButton = document.querySelector('.toggle-square')!;
 
-  document.onclick = (event: MouseEvent) => {
-    element.style.transform = `translateY(${event.clientY - element.clientHeight / 2}px) translateX(${event.clientX - element.clientWidth / 2}px)`;
-  };
+  // document.onclick = (event: MouseEvent) => {
+  //   // element.style.transform = `translateY(${event.clientY - element.clientHeight / 2}px) translateX(${event.clientX - element.clientWidth / 2}px)`;
+  // };
 
-  document.ontransitionrun = (event: TransitionEvent) => {
-    // console.log(event);
-  };
+  // document.ontransitionrun = (event: TransitionEvent) => {
+  //   // console.log(event);
+  // };
 
-  document.ontransitionstart = (event: TransitionEvent) => {
-    // console.log(event);
-  };
+  // document.ontransitionstart = (event: TransitionEvent) => {
+  //   // console.log(event);
+  // };
 
-  document.ontransitionend = (event: TransitionEvent) => {
-    // if (event.propertyName === 'transform') {
-    //   element.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    // }
-  };
+  // document.ontransitionend = (event: TransitionEvent) => {
+  //   // if (event.propertyName === 'transform') {
+  //   //   element.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  //   // }
+  // };
+
+  toggleButton.addEventListener('click', () => {
+    element.classList.toggle('hide');
+  });
 });
