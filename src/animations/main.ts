fe1203145053f6b95f5c3cae0e8560cb296ce833
Francisco <javiermartincuenca@gmail.com>
2024-06-27 22:37:04 +0200

    Animations javascript

--- src/animations/main.ts
+++ src/animations/main.ts
@@ -1,1 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector('.square')!
+  element.addEventListener('animationstart', (e) => {
+
+  })
+  element.addEventListener('animationend', (e) => {
+    
+  })
+  element.addEventListener('animationiteration', (e) => {
+    
+  })
+  element.addEventListener('animationcancel', (e) => {
+    
+  })
+});
