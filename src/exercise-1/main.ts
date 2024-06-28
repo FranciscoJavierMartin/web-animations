11a325fbef5854b4bdc9c6d6427eb47e2d7f4ee1
Francisco <javiermartincuenca@gmail.com>
2024-06-28 19:57:42 +0200

    Jump when press space key

--- src/exercise-1/main.ts
+++ src/exercise-1/main.ts
@@ -1,1 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const character = document.querySelector('.character');
+
+  document.addEventListener('keyup', (event) => {
+    if (event.code === 'Space' && !character?.classList.contains('jump')) {
+      character?.classList.add('jump');
+    }
+  });
+
+  character?.addEventListener('animationend', (event) => {
+    if (event.animationName === 'jump') {
+      character?.classList.remove('jump');
+    }
+  });
+});
