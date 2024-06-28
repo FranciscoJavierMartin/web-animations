b832524eeebbf7928fdfb7dbabfe4f324eebc76a
Francisco <javiermartincuenca@gmail.com>
2024-06-28 22:33:38 +0200

    Another way to define keyframes

--- src/web-api/main.ts
+++ src/web-api/main.ts
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.querySelector('.square');
   const squareAnimation = element?.animate(
-    [
-      {
-        transform: 'translateX(0)',
-      },
-      {
-        backgroundColor: 'blue',
-        offset: 0.8,
-      },
-      {
-        transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
-        backgroundColor: 'crimson',
-      },
-    ],
+    {
+      transform: [
+        'translateX(0)',
+        'translateX(calc(100vw - 100px)) rotate(360deg)',
+      ],
+      backgroundColor: ['gold', 'blue', 'crimson'],
+      offset: [0, 0.3, 1],
+      easing: ['ease-in', 'linear'],
+      composite: ['add', 'replace', 'add'],
+    },
+    // [
+    // {
+    //   transform: 'translateX(0)',
+    //   easing: 'ease-in',
+    // },
+    // {
+    //   backgroundColor: 'blue',
+    //   offset: 0.8,
+    //   composite: 'replace',
+    // },
+    // {
+    //   transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
+    //   backgroundColor: 'crimson',
+    // },
+    // ],
     {
       duration: 3000,
       delay: 1000,
