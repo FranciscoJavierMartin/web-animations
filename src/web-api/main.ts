c24f558268ef98ce4a45180de503942cfc9b050d
Francisco <javiermartincuenca@gmail.com>
2024-06-29 11:47:21 +0200

    buttons handler

--- src/web-api/main.ts
+++ src/web-api/main.ts
@@ -1,37 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.querySelector('.square');
   const squareAnimation = element?.animate(
-    {
-      transform: [
-        'translateX(0)',
-        'translateX(calc(100vw - 100px)) rotate(360deg)',
-      ],
-      backgroundColor: ['gold', 'blue', 'crimson'],
-      offset: [0, 0.3, 1],
-      easing: ['ease-in', 'linear'],
-      composite: ['add', 'replace', 'add'],
-    },
-    // [
     // {
-    //   transform: 'translateX(0)',
-    //   easing: 'ease-in',
+    //   transform: [
+    //     'translateX(0)',
+    //     'translateX(calc(100vw - 100px)) rotate(360deg)',
+    //   ],
+    //   backgroundColor: ['gold', 'blue', 'crimson'],
+    //   offset: [0, 0.3, 1],
+    //   easing: ['ease-in', 'linear'],
+    //   composite: ['add', 'replace', 'add'],
     // },
-    // {
-    //   backgroundColor: 'blue',
-    //   offset: 0.8,
-    //   composite: 'replace',
-    // },
-    // {
-    //   transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
-    //   backgroundColor: 'crimson',
-    // },
-    // ],
+    [
+      {
+        transform: 'translateX(0)',
+        easing: 'ease-in',
+      },
+      {
+        backgroundColor: 'blue',
+        offset: 0.8,
+        composite: 'replace',
+      },
+      {
+        transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
+        backgroundColor: 'crimson',
+      },
+    ],
     {
       duration: 3000,
       delay: 1000,
       direction: 'alternate',
       fill: 'both',
-      iterations: 2,
+      iterations: 1,
       easing: 'linear',
       composite: 'add',
       iterationComposite: 'accumulate',
@@ -67,5 +67,41 @@ document.addEventListener('DOMContentLoaded', () => {
   //   squareAnimationKeyframes,
   //   document.timeline,
   // );
-  // squareAnimation.play();
+
+  squareAnimation?.pause();
+
+  const buttons = document.querySelectorAll('.button');
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (button.classList.contains('play')) {
+        squareAnimation?.play();
+      }
+
+      if (button.classList.contains('pause')) {
+        squareAnimation?.pause();
+      }
+
+      if (button.classList.contains('cancel')) {
+        squareAnimation?.cancel();
+      }
+
+      if (button.classList.contains('reverse')) {
+        squareAnimation?.reverse();
+      }
+
+      if (button.classList.contains('finish')) {
+        squareAnimation?.finish();
+      }
+    });
+  });
+
+  const playbackRateInput = document.getElementById('playbackRateInput');
+  const playbackRateInputValue = document.getElementById(
+    'playbackRateInputValue',
+  );
+
+  playbackRateInput?.addEventListener('input', (e) => {
+    squareAnimation?.updatePlaybackRate(e.target.value);
+    playbackRateInputValue.value = e.target.value;
+  });
 });
