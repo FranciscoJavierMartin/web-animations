37920d8a5f504dce3b07a92d194a5815c120d6b0
Francisco <javiermartincuenca@gmail.com>
2024-06-29 22:20:15 +0200

    Jump animation

--- src/exercise-2/main.ts
+++ src/exercise-2/main.ts
@@ -1,1 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('hello');
+  const character = document.querySelector('.character')!;
+  const characterAnimation = character.animate(
+    [
+      {
+        backgroundPosition: '0 0',
+      },
+      {
+        backgroundPosition: 'calc(var(--char-width) * -7) 0',
+      },
+    ],
+    {
+      duration: 1000,
+      iterations: Infinity,
+      easing: 'steps(8, jump-none)',
+    },
+  );
+
+  async function jump(): Promise<void> {
+    if (
+      !character.getAnimations().some((animation) => animation.id === 'jump')
+    ) {
+      characterAnimation.pause();
+      character.classList.add('jump');
+      const jumpAnimation = character.animate(
+        [
+          {
+            transform: 'translateY(0)',
+          },
+          {
+            transform: 'translateY(-70px)',
+          },
+        ],
+        {
+          id: 'jump',
+          duration: 500,
+          iterations: 2,
+          direction: 'alternate',
+          easing: 'ease-in-out',
+        },
+      );
+
+      await jumpAnimation.finished;
+      character.classList.remove('jump');
+      characterAnimation.play();
+    }
+  }
+
+  document.addEventListener('keyup', (event) => {
+    switch (event.code) {
+      case 'ArrowUp':
+        jump();
+        break;
+      case 'ArrowRight':
+        break;
+      case 'ArrowLeft':
+        break;
+
+      case 'Space':
+        break;
+
+      default:
+        break;
+    }
+  });
+});
