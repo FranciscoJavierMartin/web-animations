9041b8916bbda22f9e126937243573c59ab068a0
Francisco <javiermartincuenca@gmail.com>
2024-06-30 19:09:19 +0200

    Timeline in JS

--- src/scroll-animations/main.ts
+++ src/scroll-animations/main.ts
@@ -1,1 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const container1 = document.querySelector('.c-1')!;
+  const container2 = document.querySelector('.c-2')!;
+
+  const timeline = new ScrollTimeline({
+    source: container2,
+    axis: 'block',
+  });
+
+  container1.animate(
+    [
+      {
+        backgroundColor: 'salmon',
+      },
+    ],
+    {
+      fill: 'both',
+      rangeStart: '30%',
+      rangeEnd: '70%',
+      timeline,
+    },
+  );
+});
