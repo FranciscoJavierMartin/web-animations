fbb948b64853b38373f35cf303fe30e771fa7366
Francisco <javiermartincuenca@gmail.com>
2024-06-30 19:33:51 +0200

    More progress with JS

--- src/scroll-animations/main.ts
+++ src/scroll-animations/main.ts
@@ -1,11 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container1 = document.querySelector('.c-1')!;
   const container2 = document.querySelector('.c-2')!;
+  const progress = document.querySelector('.c-1 .progress .progress-inner')!;
 
   const timeline = new ScrollTimeline({
     source: container2,
     axis: 'block',
   });
+
+  const viewTimeline = new ViewTimeline({
+    subject: progress,
+    axis: 'block',
+    inset: ['auto', CSS.px(100)],
+  });
+
+  progress.animate(
+    [
+      {
+        width: 0,
+      },
+      {
+        width: '100%',
+      },
+    ],
+    {
+      fill: 'both',
+      timeline: viewTimeline,
+      rangeStart: {
+        rangeName: 'cover',
+        offset: CSS.percent(30),
+      },
+    },
+  );
 
   container1.animate(
     [
@@ -15,9 +41,9 @@ document.addEventListener('DOMContentLoaded', () => {
     ],
     {
       fill: 'both',
-      rangeStart: '30%',
-      rangeEnd: '70%',
-      timeline,
+      // rangeStart: '30%',
+      // rangeEnd: '70%',
+      timeline: viewTimeline,
     },
   );
 });
