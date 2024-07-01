5c0674056c9903474b7d364c7fe7e2b824388195
Francisco <javiermartincuenca@gmail.com>
2024-07-01 21:51:43 +0200

    startViewTransition

--- src/view-transitions/main.ts
+++ src/view-transitions/main.ts
@@ -33,10 +33,23 @@ document.addEventListener('DOMContentLoaded', () => {
   grid.addEventListener('click', async (e) => {
     const item = e.target.closest('.grid-item');
     if (!item || item.classList.contains('active')) return;
-    expandImage(item);
+
+    if (document.startViewTransition) {
+      document.startViewTransition(() => {
+        expandImage(item);
+      });
+    } else {
+      expandImage(item);
+    }
   });
 
   gridButton.addEventListener('click', async (e) => {
-    displayGrid();
+    if (document.startViewTransition) {
+      document.startViewTransition(() => {
+        displayGrid();
+      });
+    } else {
+      displayGrid();
+    }
   });
 });
