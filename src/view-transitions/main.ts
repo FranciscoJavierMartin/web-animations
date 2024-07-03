06ab7a9d8ff1fda92843d1fc306aa0e23f8d7324
Francisco <javiermartincuenca@gmail.com>
2024-07-03 18:59:40 +0200

    Animating grid items with dynamic view transition names

--- src/view-transitions/main.ts
+++ src/view-transitions/main.ts
@@ -5,6 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const main = document.querySelector('.main .item')!;
   const gridButton = document.querySelector('.grid-view-button')!;
 
+  function populateGridItemViewTransitionName(clear?: boolean): void {
+    grid.querySelectorAll('.grid-item').forEach((item, index) => {
+      item.style.viewTransitionName = clear ? 'none' : `grid-item-${index}`;
+    });
+  }
+
+  populateGridItemViewTransitionName();
+
   function expandImage(item) {
     const title = item.querySelector('h3').innerText;
     const largeImage = item.dataset.largeImage;
@@ -37,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!item || item.classList.contains('active')) return;
 
     if (document.startViewTransition) {
-      const thumbnail = item.querySelector('img')!;
+      const thumbnail = item.querySelector('img');
       const largeImage = main.querySelector('img')!;
 
       thumbnail.style.viewTransitionName = 'image';
@@ -50,6 +58,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });
 
       await transition.finished;
+      populateGridItemViewTransitionName(true);
+
       // largeImage.style.viewTransitionName = 'none';
 
       item.scrollIntoView({
@@ -63,6 +73,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.startViewTransition) {
       const activeThumbnail = grid.querySelector('.active img');
       const largeImage = main.querySelector('img')!;
+
+      populateGridItemViewTransitionName();
 
       const transition = document.startViewTransition(() => {
         activeThumbnail.style.viewTransitionName = 'image';
